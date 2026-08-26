cd2d23cce6365da832b84f02d3b45c3d0c7a55bb
KarolinaLeikam <leikamkarolina@gmail.com>
2026-08-26 16:52:55 +0200

    feed changed

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -2,6 +2,7 @@ import HeaderFeed from '@/pages/Feed/components/HeaderFeed/HeaderFeed';
 import Footer from '@/components/common/Footer/Footer';
 import StatusBar from '@/components/common/StatusBar/StatusBar';
 import StoriesHeader from '@/pages/Feed/components/StoriesHeader/StoriesHeader';
+import PostFriend from '@/components/common/PostFriend/PostFriend';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '@/context/AuthContext';
 import styles from './Feed.module.scss';
